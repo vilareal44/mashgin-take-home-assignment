4bec2e682bb0d46a1a2b446c63f828c18cef01b3
Henrique Vila Real <henrique@codebase.com.br>
2025-02-27 17:46:24 -0300

    docs(api): add bootstrap function documentation comment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+/**
+ * Bootstrap the NestJS application
+ *  
+ */
 async function bootstrap() {
   const port = process.env.PORT ?? 3001;
   const app = await NestFactory.create(AppModule);
