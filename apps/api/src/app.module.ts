d26ef2d77fb3e2526709c6a4a507c72ea5990eef
Henrique Vila Real <henrique@codebase.com.br>
2025-02-28 00:19:38 -0300

    feat(api): add static asset (images)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,26 @@ import { SharedModule } from './shared/shared.module';
 import { PrismaService } from './shared/prisma.service';
 import { MenuModule } from './menu/menu.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [SharedModule, MenuModule, CheckoutModule],
+  imports: [
+    SharedModule,
+    MenuModule,
+    CheckoutModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'assets'),
+      serveRoot: '/assets',
+      serveStaticOptions: {
+        index: false,
+        fallthrough: true,
+        extensions: ['jpg'],
+        cacheControl: true, // cache assets for improve performance on the browser
+        maxAge: 3600000, // 1 hour in milliseconds
+      }
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
