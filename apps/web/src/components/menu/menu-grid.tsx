2d3e7844a62712f652285e2f47788c26186bb0f8
Henrique Vila Real <henrique@codebase.com.br>
2025-03-01 20:09:22 -0300

    refactor(web): simplify menu grid loading and error handling

--- apps/web/src/components/menu/menu-grid.tsx
+++ apps/web/src/components/menu/menu-grid.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 
 import { ErrorAlert } from "./error-alert";
 import { MenuItemCard } from "./menu-item-card";
 import { useMenuStore } from "@/lib/store/menu-store";
+import { LoadingSpinner } from "../ui/loading-spinner";
 
 /**
  * Component that displays a grid of menu items
@@ -13,34 +14,16 @@ import { useMenuStore } from "@/lib/store/menu-store";
 export function MenuGrid() {
   const { menuItems, loading, error, fetchMenuItems } = useMenuStore();
 
-  const handleFetchMenuItems = useCallback(() => {
+  useEffect(() => {
     fetchMenuItems();
   }, [fetchMenuItems]);
 
-  useEffect(() => {
-    handleFetchMenuItems();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   if (loading) {
-    return (
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
-      </div>
-    );
+    return <LoadingSpinner className="w-10 h-10 mx-auto" />;
   }
 
   if (error) {
-    return (
-      <div>
-        <ErrorAlert message={error} onRetry={handleFetchMenuItems} />
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {menuItems.map((item) => (
-            <MenuItemCard key={item.id} item={item} />
-          ))}
-        </div>
-      </div>
-    );
+    return <ErrorAlert message={error} onRetry={fetchMenuItems} />
   }
 
   return (
@@ -50,4 +33,4 @@ export function MenuGrid() {
       ))}
     </div>
   );
-} 
+}
